83a6d7930c040ea09f5f5a8b677ba03ba0ec0190
Shirley <58549322+Shirley924@users.noreply.github.com>
2021-03-04 13:15:17 +0800

    Add files via upload
    DIY Pizza

---
+++ practice5-1/practice5-1/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace practice5_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.Sauce = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(346, 45);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(84, 19);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Blueberry";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // Sauce
+            // 
+            this.Sauce.AutoSize = true;
+            this.Sauce.Location = new System.Drawing.Point(346, 13);
+            this.Sauce.Name = "Sauce";
+            this.Sauce.Size = new System.Drawing.Size(40, 15);
+            this.Sauce.TabIndex = 1;
+            this.Sauce.Text = "Sauce";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(346, 71);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(70, 19);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Matcha";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(346, 97);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(72, 19);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Tomato";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(346, 123);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(58, 19);
+            this.radioButton4.TabIndex = 4;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "None";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(346, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(349, 232);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(84, 19);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Pineapple";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(349, 269);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(56, 19);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.Text = "Ham";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(457, 226);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(457, 269);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 25);
+            this.textBox2.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(564, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 15);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Piece(s)";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(567, 278);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 15);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Piece(s)";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(346, 312);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(262, 57);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Make";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Image = global::practice5_1.Properties.Resources.SAUCE_BLUE;
+            this.pictureBox2.Location = new System.Drawing.Point(21, 29);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(223, 222);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 14;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::practice5_1.Properties.Resources.BREAD;
+            this.pictureBox1.Location = new System.Drawing.Point(33, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(269, 271);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 401);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.Sauce);
+            this.Controls.Add(this.radioButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label Sauce;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ practice5-1/practice5-1/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practice5_1
+{
+    public partial class Form1 : Form
+    {
+        //Object Background=new Bitmap("..\\..\\IMG\\BREAD.png");
+        int sauce=0;
+        int pin = 0;
+        int h = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox2.Hide();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            sauce = 1;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            sauce = 2;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            sauce = 3;
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            sauce = 0;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //sauce
+            pictureBox2.BackColor = Color.Transparent;
+            pictureBox2.Show();
+            switch (sauce)
+            {
+                case 1:
+                    pictureBox2.Image = Properties.Resources.SAUCE_BLUE;
+                    break;
+                case 2:
+                    pictureBox2.Image = Properties.Resources.SAUCE_GREEN;
+                    break;
+                case 3:
+                    pictureBox2.Image = Properties.Resources.SAUCE_RED;
+                    break;
+                case 0:
+                    pictureBox2.Image = Properties.Resources.SAUCE_NONE;
+                    break;
+            }
+            pictureBox1.Controls.Add(pictureBox2);
+
+            Random ranObj = new Random();
+            if (pin%2 == 1)
+            {
+                PictureBox[] pineapple = new PictureBox[int.Parse(textBox1.Text)];
+                for (int i = 0; i < int.Parse(textBox1.Text); i++)
+                {
+                    int rnd_x, rnd_y;
+                    while (true)
+                    {
+                        Screen now = Screen.FromHandle(this.Handle);
+                        rnd_x = ranObj.Next(now.Bounds.Width);
+                        rnd_y = ranObj.Next(now.Bounds.Height);
+                        double length = Math.Sqrt(Math.Pow((rnd_x - 74), 2) + Math.Pow((rnd_y - 78), 2));
+                        if (length < 80)
+                            break;
+                    }
+                    pineapple[i] = new System.Windows.Forms.PictureBox();
+                    pineapple[i].BorderStyle = BorderStyle.None;
+                    pineapple[i].Size = new Size(17, 17);
+                    pineapple[i].Location = new Point(rnd_x, rnd_y);
+                    pineapple[i].SizeMode = PictureBoxSizeMode.Zoom;
+                    pineapple[i].Image = Properties.Resources.PINEAPPLE;
+                    pineapple[i].BackColor = Color.Transparent;
+
+                    pictureBox2.Controls.Add(pineapple[i]);
+                }
+            }
+
+            if (h%2 == 1)
+            {
+                PictureBox[] ham = new PictureBox[int.Parse(textBox2.Text)];
+                for (int i = 0; i < int.Parse(textBox2.Text); i++)
+                {
+                    int rnd_x, rnd_y;
+                    while (true)
+                    {
+                        Screen now = Screen.FromHandle(this.Handle);
+                        rnd_x = ranObj.Next(now.Bounds.Width);
+                        rnd_y = ranObj.Next(now.Bounds.Height);
+                        double length = Math.Sqrt(Math.Pow((rnd_x - 74), 2) + Math.Pow((rnd_y - 78), 2));
+                        if (length < 80)
+                            break;
+                    }
+                    ham[i] = new System.Windows.Forms.PictureBox();
+                    ham[i].BorderStyle = BorderStyle.None;
+                    ham[i].Size = new Size(17, 17);
+                    ham[i].Location = new Point(rnd_x, rnd_y);
+                    //ham[i].Location = new Point(31 + i * 20, 39 + i * 20);
+                    ham[i].SizeMode = PictureBoxSizeMode.Zoom;
+                    ham[i].Image = Properties.Resources.HAM;
+                    ham[i].BackColor = Color.Transparent;
+
+                    pictureBox2.Controls.Add(ham[i]);
+                }
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            pin++;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            h++;
+        }
+    }
+}
