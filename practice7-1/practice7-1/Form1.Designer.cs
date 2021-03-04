5800b7a0af08466eb7c951c960a890ebfb75ef80
Shirley <58549322+Shirley924@users.noreply.github.com>
2021-03-04 13:31:25 +0800

    Add files via upload
    模擬等化器

---
+++ practice7-1/practice7-1/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace practice7_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.hScrollBar3 = new System.Windows.Forms.HScrollBar();
+            this.hScrollBar2 = new System.Windows.Forms.HScrollBar();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(-1, 1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(840, 580);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(832, 551);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(745, 493);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(746, 440);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Start);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.pictureBox1);
+            this.tabPage2.Controls.Add(this.hScrollBar3);
+            this.tabPage2.Controls.Add(this.hScrollBar2);
+            this.tabPage2.Controls.Add(this.hScrollBar1);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(832, 551);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(213, 43);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(195, 86);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // hScrollBar3
+            // 
+            this.hScrollBar3.Location = new System.Drawing.Point(192, 327);
+            this.hScrollBar3.Name = "hScrollBar3";
+            this.hScrollBar3.Size = new System.Drawing.Size(250, 21);
+            this.hScrollBar3.TabIndex = 6;
+            // 
+            // hScrollBar2
+            // 
+            this.hScrollBar2.Location = new System.Drawing.Point(192, 251);
+            this.hScrollBar2.Name = "hScrollBar2";
+            this.hScrollBar2.Size = new System.Drawing.Size(250, 21);
+            this.hScrollBar2.TabIndex = 5;
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(192, 191);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(250, 21);
+            this.hScrollBar1.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label4.Location = new System.Drawing.Point(54, 327);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "藍色";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(54, 257);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "綠色";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(54, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "紅色";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "顏色設定";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 95;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 110;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Enabled = true;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(680, 30);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "選擇音樂";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(837, 581);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.background);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.HScrollBar hScrollBar3;
+        private System.Windows.Forms.HScrollBar hScrollBar2;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ practice7-1/practice7-1/Form1.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Security;
+using System.Diagnostics;
+
+namespace practice7_1
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        System.Media.SoundPlayer music = new System.Media.SoundPlayer();
+        string filePath= "awaken.wav";
+        TrackBar[] trcb = new TrackBar[10];
+        PictureBox[,] lab = new PictureBox[10, 10];
+        int[] judge = new int[3];
+
+        private void background(object sender, EventArgs e)
+        {
+            //調音量
+            for(int i = 0; i < 10; i++)
+            {
+                trcb[i] = new TrackBar();
+                trcb[i].Orientation = Orientation.Vertical;
+                trcb[i].Minimum = 0;
+                trcb[i].Maximum = 9;
+                trcb[i].TickFrequency = 1;
+                trcb[i].Location = new Point(10+55*i, 250);
+                trcb[i].Size = new Size(10,150);
+                tabPage1.Controls.Add(trcb[i]);
+            }
+
+            //volumn
+            for(int i = 0; i < 10; i++)
+            {
+                for(int j = 0; j < 10; j++)
+                {
+                    lab[i, j] = new PictureBox();
+                    lab[i, j].BackColor = Color.LightGray;
+                    lab[i, j].Size = new Size(45, 15);
+                    lab[i, j].Location = new Point(10 + 55 * i, 50 + 20 * j);
+                    tabPage1.Controls.Add(lab[i, j]);
+                }
+            }
+
+            hScrollBar1.Minimum = 0;
+            hScrollBar1.Maximum = 255;
+            hScrollBar2.Minimum = 0;
+            hScrollBar2.Maximum = 255;
+            hScrollBar3.Minimum = 0;
+            hScrollBar3.Maximum = 255;
+
+            
+            
+        }
+
+        private void Start(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            timer3.Enabled = true;
+            
+            music.SoundLocation = @filePath;
+            music.PlayLooping();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {            
+            judge[0]++;
+            int[] value = new int[3];
+            value[0] = 9-trcb[0].Value;
+            value[1] = 9-trcb[3].Value;
+            value[2] = 9-trcb[8].Value;
+
+            for(int i = 0; i < value[0]; i++)
+                lab[0, i].BackColor = pictureBox1.BackColor;
+            for (int i = 9; i > value[0]; i--)
+                lab[0, i].BackColor = Color.White;
+
+            for (int i = 0; i < value[1]; i++)
+                lab[3, i].BackColor = pictureBox1.BackColor;
+            for (int i = 9; i > value[1]; i--)
+                lab[3, i].BackColor = Color.White;
+
+            for (int i = 0; i < value[2]; i++)
+                lab[8, i].BackColor = pictureBox1.BackColor;
+            for (int i = 9; i > value[2]; i--)
+                lab[8, i].BackColor = Color.White;
+
+            if (judge[0] % 2 == 0)
+            {
+                if (value[0] > 0)
+                    lab[0, value[0] - 1].BackColor = Color.White;
+                if (value[1] > 0)
+                    lab[3, value[1] - 1].BackColor = pictureBox1.BackColor;
+                if (value[2] > 0)
+                    lab[8, value[2] - 1].BackColor = Color.White;
+
+                lab[0, value[0]].BackColor = pictureBox1.BackColor;
+                lab[3, value[1]].BackColor = Color.White;
+                lab[8, value[2]].BackColor = pictureBox1.BackColor;
+
+                if (value[0] < 9)
+                    lab[0, value[0] + 1].BackColor = Color.White;
+                if (value[1] < 9)
+                    lab[3, value[1] + 1].BackColor = pictureBox1.BackColor;
+                if (value[2] < 9)
+                    lab[8, value[2] + 1].BackColor = Color.White;
+            }
+            else
+            {
+                if (value[0] > 0)
+                    lab[0, value[0] - 1].BackColor = pictureBox1.BackColor;
+                if (value[1] > 0)
+                    lab[3, value[1] - 1].BackColor = Color.White;
+                if (value[2] > 0)
+                    lab[8, value[2] - 1].BackColor = pictureBox1.BackColor;
+
+                lab[0, value[0]].BackColor = Color.White;
+                lab[3, value[1]].BackColor = pictureBox1.BackColor;
+                lab[8, value[2]].BackColor = Color.White;
+
+                if (value[0] < 9)
+                    lab[0, value[0] + 1].BackColor = pictureBox1.BackColor;
+                if (value[1] < 9)
+                    lab[3, value[1] + 1].BackColor = Color.White;
+                if (value[2] < 9)
+                    lab[8, value[2] + 1].BackColor = pictureBox1.BackColor;
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            judge[1]++;
+            int[] value = new int[3];
+            value[0] = 9-trcb[1].Value;
+            value[1] = 9-trcb[5].Value;
+            value[2] = 9-trcb[7].Value;
+
+            for (int i = 0; i < value[0]; i++)
+                lab[1, i].BackColor = pictureBox1.BackColor;
+            for (int i = 9; i > value[0]; i--)
+                lab[1, i].BackColor = Color.White;
+
+            for (int i = 0; i < value[1]; i++)
+                lab[5, i].BackColor = pictureBox1.BackColor;
+            for (int i = 9; i > value[1]; i--)
+                lab[5, i].BackColor = Color.White;
+
+            for (int i = 0; i < value[2]; i++)
+                lab[7, i].BackColor = pictureBox1.BackColor;
+            for (int i = 9; i > value[2]; i--)
+                lab[7, i].BackColor = Color.White;
+
+            if (judge[1] % 2 == 0)
+            {
+                if (value[0] > 0)
+                    lab[1, value[0] - 1].BackColor = Color.White;
+                if (value[1] > 0)
+                    lab[5, value[1] - 1].BackColor = pictureBox1.BackColor;
+                if (value[2] > 0)
+                    lab[7, value[2] - 1].BackColor = Color.White;
+
+                lab[1, value[0]].BackColor = pictureBox1.BackColor;
+                lab[5, value[1]].BackColor = Color.White;
+                lab[7, value[2]].BackColor = pictureBox1.BackColor;
+
+                if (value[0] < 9)
+                    lab[1, value[0] + 1].BackColor = Color.White;
+                if (value[1] < 9)
+                    lab[5, value[1] + 1].BackColor = pictureBox1.BackColor;
+                if (value[2] < 9)
+                    lab[7, value[2] + 1].BackColor = Color.White;
+            }
+            else
+            {
+                if (value[0] > 0)
+                    lab[1, value[0] - 1].BackColor = pictureBox1.BackColor;
+                if (value[1] > 0)
+                    lab[5, value[1] - 1].BackColor = Color.White;
+                if (value[2] > 0)
+                    lab[7, value[2] - 1].BackColor = pictureBox1.BackColor;
+
+                lab[1, value[0]].BackColor = Color.White;
+                lab[5, value[1]].BackColor = pictureBox1.BackColor;
+                lab[7, value[2]].BackColor = Color.White;
+
+                if (value[0] < 9)
+                    lab[1, value[0] + 1].BackColor = pictureBox1.BackColor;
+                if (value[1] < 9)
+                    lab[5, value[1] + 1].BackColor = Color.White;
+                if (value[2] < 9)
+                    lab[7, value[2] + 1].BackColor = pictureBox1.BackColor;
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            judge[2]++;
+            int[] value = new int[4];
+            value[0] = 9-trcb[2].Value;
+            value[1] = 9-trcb[4].Value;
+            value[2] = 9-trcb[6].Value;
+            value[3] = 9-trcb[9].Value;
+
+            for (int i = 0; i < value[0]-1; i++)
+                lab[2, i].BackColor = pictureBox1.BackColor;
+            for (int i = 9; i > value[0]-1; i--)
+                lab[2, i].BackColor = Color.White;
+
+            for (int i = 0; i < value[1]-1; i++)
+                lab[4, i].BackColor = pictureBox1.BackColor;
+            for (int i = 9; i > value[1]-1; i--)
+                lab[4, i].BackColor = Color.White;
+
+            for (int i = 0; i < value[2]-1; i++)
+                lab[6, i].BackColor = pictureBox1.BackColor;
+            for (int i = 9; i > value[2]-1; i--)
+                lab[6, i].BackColor = Color.White;
+
+            for (int i = 0; i < value[3]-1; i++)
+                lab[9, i].BackColor = pictureBox1.BackColor;
+            for (int i = 9; i > value[3]-1; i--)
+                lab[9, i].BackColor = Color.White;
+
+            if (judge[2] % 2 == 0)
+            {
+                if (value[0] > 0) 
+                    lab[2, value[0]-1].BackColor = Color.White;
+                if (value[1] > 0) 
+                    lab[4, value[1]-1].BackColor = pictureBox1.BackColor;
+                if (value[2] > 0) 
+                    lab[6, value[2]-1].BackColor = Color.White;
+                if (value[3] > 0) 
+                    lab[9, value[3]-1].BackColor = pictureBox1.BackColor;
+
+                lab[2, value[0]].BackColor = pictureBox1.BackColor;
+                lab[4, value[1]].BackColor = Color.White;
+                lab[6, value[2]].BackColor = pictureBox1.BackColor;
+                lab[9, value[3]].BackColor = Color.White;
+
+                if (value[0] < 9) 
+                    lab[2, value[0]+1].BackColor = Color.White;
+                if (value[1] < 9) 
+                    lab[4, value[1]+1].BackColor = pictureBox1.BackColor;
+                if (value[2] < 9) 
+                    lab[6, value[2]+1].BackColor = Color.White;
+                if (value[3] < 9) 
+                    lab[9, value[3]+1].BackColor = pictureBox1.BackColor;
+            }
+            else
+            {
+                if (value[0] > 0) 
+                    lab[2, value[0]-1].BackColor = pictureBox1.BackColor;
+                if (value[1] > 0) 
+                    lab[4, value[1]-1].BackColor = Color.White;
+                if (value[2] > 0) 
+                    lab[6, value[2]-1].BackColor = pictureBox1.BackColor;
+                if (value[3] > 0) 
+                    lab[9, value[3]-1].BackColor = Color.White;
+
+                lab[2, value[0]].BackColor = Color.White;
+                lab[4, value[1]].BackColor = pictureBox1.BackColor;
+                lab[6, value[2]].BackColor = Color.White;
+                lab[9, value[3]].BackColor = pictureBox1.BackColor;
+
+                if (value[0] < 9) 
+                    lab[2, value[0]+1].BackColor = pictureBox1.BackColor;
+                if (value[1] < 9)
+                    lab[4, value[1]+1].BackColor = Color.White;
+                if (value[2] < 9)
+                    lab[6, value[2]+1].BackColor = pictureBox1.BackColor;
+                if (value[3] < 9)
+                    lab[9, value[3]+1].BackColor = Color.White;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            timer3.Enabled = false;
+            
+            music.Stop();
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.BackColor = Color.FromArgb(hScrollBar1.Value, hScrollBar2.Value, hScrollBar3.Value);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //選擇檔案路徑
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "Media Files|*.wav*";
+            //file.ShowDialog();
+            //this.txtFile.Text = file.SafeFileName;
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    filePath = file.FileName;
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+            }
+        }
+    
+    }
+}
